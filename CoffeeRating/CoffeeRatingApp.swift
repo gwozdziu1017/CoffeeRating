37a87713fe164534703cf0cb2c8a344f59e933f8
Damian <gwozdziu1017@o2.pl>
2025-06-25 10:00:18 +0200

    Adding project directory

---
+++ CoffeeRating/CoffeeRatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeRatingApp.swift
+//  CoffeeRating
+//
+//  Created by Damian Gwóźdź on 25/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeRatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
