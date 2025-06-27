fbfbfc441c682522a38b620543cf6c811af3b401
Damian <gwozdziu1017@o2.pl>
2025-06-27 14:26:08 +0200

    Adding MapService and UserRatingService

---
+++ CoffeeRating/MapService.swift
@@ -0,0 +1,9 @@
+//
+//  MapService.swift
+//  CoffeeRating
+//
+//  Created by Damian Gwóźdź on 27/06/2025.
+//
+
+// fetch nearby coffee places coordinates
+// show place details
---
+++ CoffeeRating/UserRatingService.swift
@@ -0,0 +1,11 @@
+//
+//  UserRatingService.swift
+//  CoffeeRating
+//
+//  Created by Damian Gwóźdź on 27/06/2025.
+//
+import Foundation
+
+// newRating
+// updateRating
+
