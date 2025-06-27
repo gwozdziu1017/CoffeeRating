182805c7711ea904e745c7639fdf8448393a3013
Damian <gwozdziu1017@o2.pl>
2025-06-27 09:59:19 +0200

    Adding UserRatingModel

---
+++ CoffeeRating/UserRatingModel.swift
@@ -0,0 +1,38 @@
+//
+//  UserRatingModel.swift
+//  CoffeeRating
+//
+//  Created by Damian Gwóźdź on 27/06/2025.
+//
+import Foundation
+
+class RatingModel: Identifiable, Codable {
+    var id: UUID
+    
+    let rateId: Int
+    let userId: Int
+    let coffeeShopId: Int
+    var rating: Int
+    let createdAt: Date
+    var updatedAt: Date?
+
+    enum CodingKeys: String, CodingKey {
+        case rateId
+        case userId
+        case coffeeShopId
+        case rating
+        case createdAt
+        case updatedAt
+    }
+
+    required init(from decoder: Decoder) throws {
+        id = UUID()
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        rateId = try container.decode(Int.self, forKey: .rateId)
+        userId = try container.decode(Int.self, forKey: .userId)
+        coffeeShopId = try container.decode(Int.self, forKey: .coffeeShopId)
+        rating = try container.decode(Int.self, forKey: .rating)
+        createdAt = try container.decode(Date.self, forKey: .createdAt)
+        updatedAt = try container.decodeIfPresent(Date.self, forKey: .updatedAt)
+    }
+}
