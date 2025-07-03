43c5e33cb8690369c6ea8674e05bb62d80c6cc10
Damian <gwozdziu1017@o2.pl>
2025-07-03 12:50:48 +0200

    Removing unneccessary mocks

--- CoffeeRating/CoffeeShopService.swift
+++ CoffeeRating/CoffeeShopService.swift
@@ -6,29 +6,6 @@
 //
 import Foundation
 
-let mockedCoffeeShopRatings: [Int] = [5, 5, 5, 4, 3, 1, 1]
-let mockedOpeningHours: [DaySchedule] = [
-    DaySchedule(dayOfTheWeek: "Monday", openingTime: "08:00", closingTime: "20:00"),
-    DaySchedule(dayOfTheWeek: "Tuesday", openingTime: "12:00", closingTime: "18:00")
-]
-func getMockedIsOpenNow(isOpen: Bool) -> Bool {
-    return isOpen
-}
-
-let mockedCoffeeShop: CoffeeShopModel =
-    .init(
-        name: "Starbucks",
-        address: "123 Main Street, Anytown, USA",
-        latitude: 32.6514,
-        longitude: -161.4333,
-        phoneNumber: "(123) 456-7890",
-        isOpenNow: getMockedIsOpenNow(isOpen: true),
-        openingHours: mockedOpeningHours,
-        amenities: [.wifi, .parking, .petFriendly],
-        priceRange: .moderate,
-        averageRating: getAverageRating(ratingArray: mockedCoffeeShopRatings),
-        totalNumberOfRatings: getTotalNumberOfRatings(ratingArray: mockedCoffeeShopRatings))
-
 func getAverageRating(ratingArray: [Int]) -> Double {
     return Double(ratingArray.reduce(0, +)) / Double(ratingArray.count)
 }
